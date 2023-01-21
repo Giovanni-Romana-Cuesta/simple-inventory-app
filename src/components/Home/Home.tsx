cb3d363d36c2ecef2f5e34119d97c435b70d4bd5
Giovanni Romaña <giovanni@preki.com>
2023-01-21 00:07:02 -0500

    Implement search bar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import Header from '../Header/Header';
 import { getMeters } from '../../services/services';
 import useWindowDimensions from '../../hooks/useResponsive';
@@ -11,12 +11,14 @@ import Pagination from '../Pagination/Pagination';
 
 const Home = () => {
   const [meters, setMeters] = useState<MeterModel[]>();
+  const [filteredMeters, setFilteredMeters] = useState<MeterModel[]>();
   const [error, setError] = useState<string | undefined>(undefined);
   const [page, setPage] = useState<number>(0);
   const [totalPages, setTotalPages] = useState<number>(0);
   const [nextPage, setNextPage] = useState<boolean>(false);
   const [previousPage, setPreviousPage] = useState<boolean>(false);
   const [showAddModal, setShowAddModal] = useState<boolean>(false);
+  const [searchText, setSearchText] = useState<string>('');
 
   const { isMobile } = useWindowDimensions();
 
@@ -36,6 +38,18 @@ const Home = () => {
     [page],
   );
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!meters) return;
+    setSearchText(event.target.value);
+
+    if (searchText !== '') {
+      const filteredData = meters.filter((item) => item.serial.includes(searchText));
+      setFilteredMeters(filteredData);
+    } else {
+      setFilteredMeters(meters);
+    }
+  };
+
   useEffect(() => {
     _getMeters();
   }, [getMeters]);
@@ -46,15 +60,25 @@ const Home = () => {
       <div className='home-actions'>
         <div className='search-input'>
           <i className='fa-solid fa-search'></i>
-          <input type='text' placeholder='Search' />
+          <input type='text' placeholder='Search' onChange={handleSearch} />
         </div>
         <button onClick={() => setShowAddModal(true)}>
           <i className='fas fa-plus'></i>Add Meter
         </button>
       </div>
       {error && <span>{error}</span>}
-      {isMobile && <MobileList items={meters} getMeters={_getMeters} />}
-      {!isMobile && <DesktopList items={meters} getMeters={_getMeters} />}
+      {isMobile && (
+        <MobileList
+          items={searchText.length > 1 ? filteredMeters : meters}
+          getMeters={_getMeters}
+        />
+      )}
+      {!isMobile && (
+        <DesktopList
+          items={searchText.length > 1 ? filteredMeters : meters}
+          getMeters={_getMeters}
+        />
+      )}
       <CreateMeter
         show={showAddModal}
         close={() => setShowAddModal(false)}
